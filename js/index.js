2d9f2c7591a5f4b52d6405fe8b9ef06b55238c67
Alice Won <alicewon4@gmail.com>
2020-01-13 10:21:02 -0800

    Done.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 let currentUser = {id: 1, username: "pouros"}
 
 document.addEventListener("DOMContentLoaded", function() {
+  // the only function needed in here because all other functions are linked through getBooks
   getBooks()
 });
 
@@ -11,7 +12,7 @@ const getBooks = () => {
   .then(json => showBooks(json))
 }
 
-//iterate over the array of books, take to next function
+//iterate over the array of books and create an li element for each
 const showBooks = booksArray => {
   booksArray.forEach(book => createBookLi(book))
 }
@@ -28,73 +29,62 @@ const createBookLi = book => {
 }
 
 
-// need to show only one book in show section. Currently creating a list of books that were clicked. Need to re-render book details section everytime a Book List Item is clicked, and fill with that books details
-
 const bookDetails = book => {
   // get book details and display in #show-panel
-  // title, img_url, description, users
-
-  //create title h1, image div, description p, user p
-
   let showPanel = document.getElementById('show-panel')
   console.log(showPanel)
+
+  //clear show panel before creating each new book show card
   while (showPanel.firstChild) {
     showPanel.removeChild(showPanel.firstChild)
   }
 
-
+  // create book details dynamically
   let h1 = document.createElement('h1')
   h1.innerText = book.title
-
 
   let image = document.createElement('img')
   image.src = book.img_url
  
   let pDescription = document.createElement('p')
   pDescription.innerText = book.description
- 
 
   let usersDiv = document.createElement('div')
   usersDiv.innerText ="USERS THAT LIKE THIS BOOK:"
   
-
+  //iterate over all booklikers for said book and display them in a list on show page
   console.log(book)
   book.users.forEach(user => {
-    
     userP = document.createElement('p')
     userP.innerText = user.username
     //userP.style.listStyleType = "none"
     usersDiv.appendChild(userP)
   })
   
+  // event listener for read/like button
   let readButton = document.createElement('button')
   readButton.innerText = "Read Book"
   readButton.addEventListener("click", e => {
     e.preventDefault()
     likeBook(book, e)
-    // console.log(e.target.parentNode.users)
   })
  
-  
+  //append all elmements to existing show section
   showPanel.appendChild(h1)
   showPanel.appendChild(image)
   showPanel.appendChild(pDescription)
   showPanel.appendChild(usersDiv)
-  // console.log(book.users)
   showPanel.appendChild(readButton)
   
 }
 
-// need to fix POST method
-// add current user ID to existing users/likers array and re-render the show-panel to reflect new changes with current user's ID included in list
+// patch method, update array of likers with current liker
 const likeBook = (book, e) => {
   console.log("existing array of likers:")
   console.log(usersWhoLiked = book.users)
   console.log("new array of likers:")
   console.log(usersWhoLikedPlusMe = usersWhoLiked.push(currentUser))
-
-
-
+  
   fetch(`http://localhost:3000/books/${book.id}`, {
     method: "PATCH",
     headers: {
